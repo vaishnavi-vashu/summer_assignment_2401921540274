408df59928d275041545cc988ffbdb2d153779f3
vaishnavi-vashu <vaishnavivashu.25@gmail.com>
2026-06-28 16:06:40 +0530

    Refactor twoSum function to use different map variable

--- Week_1/Day_1/Two_Sum.cpp
+++ Week_1/Day_1/Two_Sum.cpp
@@ -1,28 +1,19 @@
-#include <vector>
-#include <unordered_map>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Map to store the numbers we've seen and their indices
-        unordered_map<int, int> seen;
-        
-        for (int i = 0; i < nums.size(); ++i) {
+        unordered_map<int, int> mp; 
+
+        for (int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
-            
-            // If the complement is in our map, we found the solution
-            if (seen.find(complement) != seen.end()) {
-                return {seen[complement], i};
+           
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
             }
-            
-            // Otherwise, add the current number and its index to the map
-            seen[nums[i]] = i;
+
+            mp[nums[i]] = i;
         }
-        
-        // Return an empty vector if no solution is found 
-        // (though the problem guarantees exactly one solution)
-        return {};
+
+        return {}; 
     }
 };
+
