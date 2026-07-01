cd46c7f665b1197873309734330295e570b0b298
vaishnavi-vashu <vaishnavivashu.25@gmail.com>
2026-07-01 18:14:51 +0530

    Implement longest palindromic substring function

---
+++ Week_2/Day_5/Longest Palindromic Substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start = 0, maxLen = 1;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            // Odd-length palindrome
+            int left = i, right = i;
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+
+            // Even-length palindrome
+            left = i;
+            right = i + 1;
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
